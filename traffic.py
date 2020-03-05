5e88f1d7fed4528c0a013bf714a1836034ca3f4d
W Touchstone <wtouchstone3@gmail.com>
2020-03-04 21:41:02 -0500

    -allowed for cumulative 'path traveled' counts, modified osmnx method for coloring these

--- traffic.py
+++ traffic.py
@@ -2,31 +2,43 @@ import osmnx as ox
 import networkx as nx
 import xml.dom.minidom
 import numpy as np
-import heapq
+import pandas as pd
 from util import *  
 from pprint import pprint
 ox.utils.config(all_oneway=False)
-G = ox.graph_from_place("Ball Ground, GA", buffer_dist=500, network_type='drive')
+G = ox.graph_from_place("Atlanta", network_type='drive')
+#G = ox.graph_from_file("./data/map.osm", netwo)
 #ox.elevation.add_edge_grades(G)
-basic_stats = ox.basic_stats(G)
 #pprint(basic_stats)
 #ox.plot_graph(G)
+edgeDict = {}
 
-
-#print(closestNode(G, (33.779609,-84.4061026)))
 #print(ox.geo_utils.get_nearest_edges(G, [33.779609],[-84.4061026], dist = 100))
 #ox.save_load.save_as_osm(G, filename='tech.osm', oneway=True)
+for edge in G.edges(keys=True, data=True):
+    edge[3]['traversals'] = 0
+maxTraversals = 0
+for i in range(10000):
+    node1 = np.random.choice(G.nodes)
+    node2 = np.random.choice(G.nodes)
+    path = gmlAStar(G, node1, node2)
 
-node1 = np.random.choice(G.nodes)
-node1 = closestNode(G, (34.327687, -84.392856))
-node2 = np.random.choice(G.nodes)
-node2 = closestNode(G, (34.337557, -84.373548))
-path = gmlAStar(G, node1, node2)
-if path != []:
-    ox.plot_graph_route(G, path)
-path = nx.shortest_path(G, node1, node2)
-if path != []:
-    ox.plot_graph_route(G, path)
+    if path != []:
+        for j in range(1,len(path)):
+            edge = (path[j-1], path[j])
+            ox.geo_utils.get_route_edge_attributes(G, edge)[0]['traversals'] += 1
+            if ox.geo_utils.get_route_edge_attributes(G, edge)[0]['traversals'] > maxTraversals:
+                maxTraversals = ox.geo_utils.get_route_edge_attributes(G, edge)[0]['traversals']
+#for edge in G.edges(keys=True, data=True):
+    #edge[3]['traversals'] /= maxTraversals
+    #print(edge[3]['traversals'])
+edgeattrs = ox.graph_to_gdfs(G, nodes=False).columns
+#beep = ox.plot.get_edge_colors_by_attr(G, 'traversals', num_bins=5)
+beep = get_edge_colors_by_attribute(G, 'traversals', num_bins=250)
+ox.plot_graph(G, edge_color=beep, node_alpha=0, bgcolor='grey', edge_linewidth=4)
+#ox.plot_graph_route(G, path)
+#print(edgeDict)
+
 
 #ox.plot_graph(G)
 #route = nx.shortest_path(G, np.random.choice(G.nodes), np.random.choice(G.nodes))
--- util.py
+++ util.py
@@ -4,14 +4,22 @@ import xml.dom.minidom
 import numpy as np
 import heapq
 import copy
+import pandas as pd
 def aStarHeuristic(graph, source, dest):
-    return 110996.4513 * pow(pow((graph.nodes[source]['x'] - graph.nodes[dest]['x']),2) + pow((graph.nodes[source]['y'] - graph.nodes[dest]['y']),2),.5) #meters times lattitude at 38 degrees
+    return (1/35.0) * 110996.4513 * pow(pow((graph.nodes[source]['x'] - graph.nodes[dest]['x']),2) + pow((graph.nodes[source]['y'] - graph.nodes[dest]['y']),2),.5) #meters times lattitude at 38 degrees
 
 def closestNode(graph, location): #tuple location
     return ox.geo_utils.get_nearest_node(graph, location)
 
 def cost(G, edge):
-    return ox.geo_utils.get_route_edge_attributes(G, edge)[0]['length']
+    distance = ox.geo_utils.get_route_edge_attributes(G, edge)[0]['length']
+    try:
+        speedLimit = ox.geo_utils.get_route_edge_attributes(G, edge)[0]['maxspeed']
+        speedLimit = int(speedLimit[0:2])
+    except:
+        speedLimit = 35
+    cost = distance/speedLimit
+    return cost
 
 
 def gmlAStar(graph, source, dest): #NODE source, NODE dest, returns a path (list of node) from source to dest.
@@ -28,10 +36,8 @@ def gmlAStar(graph, source, dest): #NODE source, NODE dest, returns a path (list
         history = [source]
         totalCost = currCost + heuristic + 0
         heapq.heappush(openQueue, (totalCost, successor[1], history, currCost))
-    print(openQueue)
     while openQueue:    
         curr = heapq.heappop(openQueue)
-        print(curr)
         if curr[1] not in visitedSet:
             visitedSet.add(curr[1])
             if curr[1] == dest:
@@ -45,5 +51,20 @@ def gmlAStar(graph, source, dest): #NODE source, NODE dest, returns a path (list
                 history.append(curr[1])
                 totalCost = currCost + curr[3] + heuristic
                 heapq.heappush(openQueue, (totalCost, successor[1], history, currCost))
-    #path = unpackPath(finalDest)
-    return finalDest[2]
+    try:
+        return finalDest[2]
+    except:
+        return[]
+#this is a modified version of that of osmnx with corrected buckets for my purposes
+def get_edge_colors_by_attribute(G, attr, num_bins=5, cmap='viridis', start=0, stop=1, na_color='none'): 
+    if num_bins is None:
+            num_bins=len(G.edges())
+    bin_labels = range(num_bins)
+    attr_values = pd.Series([data[attr] for u, v, key, data in G.edges(keys=True, data=True)])
+    cats = pd.cut(x=attr_values, bins=num_bins, labels=bin_labels)
+    #cats = []
+    #for i in range(num_bins + 1):
+    #    cats.append(float(i) / num_bins)
+    colors = ox.plot.get_colors(num_bins, cmap, start, stop)
+    edge_colors = [colors[int(cat)] if pd.notnull(cat) else na_color for cat in cats]
+    return edge_colors
